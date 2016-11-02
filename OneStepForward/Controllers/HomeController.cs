f48ffa7e519cbc9aa3c374fd8231c010dd682fbd
Coman Games <alfaa.gen@gmail.com>
2016-11-02 15:37:33 +0200

    Version 0.0.1 is done

--- OneStepForward/Controllers/HomeController.cs
+++ OneStepForward/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace OneStepForward.Controllers
 {
@@ -15,16 +11,10 @@ namespace OneStepForward.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "This application is created to gain more self-discipline";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
